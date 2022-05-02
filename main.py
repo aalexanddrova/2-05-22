38cea3d0c1b60fdbaf10e55c5607389577910404
Anastasia Doroshenko <91866423+aalexanddrova@users.noreply.github.com>
2022-05-02 06:05:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+age_brother = int(input('ievadi brala vecumu: '))
+
+age_sister = int(input('ievadi masas vecumu: '))
+
+
+if age_brother > age_sister:
+
+   print('bralis vecaks par - ', age_brother - age_sister)
+
+elif age_sister > age_brother:
+
+   print('masa vecaka par - ', age_sister - age_brother)
+
+else:
+
+   print('bralis un masa ir dvini', )
